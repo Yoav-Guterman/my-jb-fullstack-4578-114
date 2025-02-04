f92847faba7defd47305d843e78e58bb715ce1f0
Yoav-Guterman <Yoav-Guterman>
2025-02-04 10:16:22 +0200

    alota

---
+++ 2025-02-04-nodejs-2/01-homework-and -error-handling/app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const axios = require('axios')
+const fs = require('fs/promises')
+
+const app = express()
+
+const getUsers = async (req, res, next) => {
+    try {
+        const response = await axios('https://jsonplaceholder.typicode.com/users')
+        res.json(response.data)
+    } catch (err) {
+        next(err)
+    }
+
+}
+
+const getUser = async (req, res, next) => {
+    try {
+        const response = await axios(`https://jsonplaceholder.typicode.com/users/${req.query.id}`)
+        res.json(response.data)
+    } catch (err) {
+
+    }
+
+}
+
+const notFound = async (req, res, next) => {
+    res.status(404).send('page not found')
+}
+
+const errorLogger = async (err, req, res, next) => {
+    await fs.appendFile('./error.log', err)
+}
+
+
+app.get('/users', getUsers)
+app.get('/user', getUser)
+app.use(notFound)
+
+app.use(errorLogger)
+
+app.listen(3000, () => console.log('started...'))
