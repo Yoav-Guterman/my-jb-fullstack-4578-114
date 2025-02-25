2a7d95d8a380e788ab02d47bf681d0836d6e9341
Yoav-Guterman <yoavguterman@gmail.com>
2025-02-25 23:06:22 +0200

    fullstack-boilerplate

--- 2025-02-25-docker/02-social-network-integration/backend/src/middlewares/error/error-logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = errorLogger;
-function errorLogger(err, req, res, next) {
-    console.error(err.message);
-    next(err);
-}
--- 2025-02-25-docker/02-social-network-integration/backend/src/routers/feed.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const controller_1 = require("../controllers/feed/controller");
-const feedRouter = (0, express_1.Router)();
-feedRouter.get('/', controller_1.getFeed);
-exports.default = feedRouter;
